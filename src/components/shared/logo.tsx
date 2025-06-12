f0b2b81eaf124b657802598a67521eb02117af49
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-06-13 00:00:54 +0600

    feat: updated logo

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -1,10 +1,17 @@
-export const Logo = () => {
+import logoImg from '@/assets/logo.png';
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+
+interface Props {
+  textClassName: string;
+  className?: string;
+}
+
+export const Logo = ({ className, textClassName }: Props) => {
   return (
-    <div className="flex items-center gap-1.5">
-      <div className="flex h-8 w-8 items-center justify-center rounded-full bg-white">
-        <div className="h-4 w-4 rounded-full bg-black" />
-      </div>
-      <span className="font-head text-xl font-bold">Kormo Setu</span>
+    <div className="flex items-center gap-x-2">
+      <Image src={logoImg} alt="Logo" className={cn(`size-10`, className)} />
+      <span className={cn('font-head', textClassName)}>Kormo Setu</span>
     </div>
   );
 };
