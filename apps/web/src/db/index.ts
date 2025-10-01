38a66deb1b60889494cb704803a43b7b65bafc38
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-10-01 09:49:57 +0600

    feat: updated company db migration

--- apps/web/src/db/index.ts
+++ apps/web/src/db/index.ts
@@ -1,4 +1,4 @@
 import { drizzle } from "drizzle-orm/neon-http";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+export const db = drizzle(process.env.DATABASE_URL as string);
 export * from "./schema";
