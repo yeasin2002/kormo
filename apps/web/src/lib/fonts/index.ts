b07445eb6121c2528e81da112c37af359d7f42fe
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-10-02 11:35:06 +0600

    fix: updated CSS file warning setting and added missing fonts

--- apps/web/src/lib/fonts/index.ts
+++ apps/web/src/lib/fonts/index.ts
@@ -1,4 +1,8 @@
-import { Archivo_Black, Space_Grotesk } from "next/font/google";
+import {
+	Archivo_Black,
+	Instrument_Serif,
+	Space_Grotesk,
+} from "next/font/google";
 
 const archivoBlack = Archivo_Black({
 	subsets: ["latin"],
@@ -14,6 +18,13 @@ const space = Space_Grotesk({
 	display: "swap",
 });
 
-const fontList = [archivoBlack, space];
+const instrumentSerif = Instrument_Serif({
+	subsets: ["latin"],
+	weight: "400",
+	variable: "--font-instrument-serif",
+	display: "swap",
+});
+
+const fontList = [archivoBlack, space, instrumentSerif];
 
 export const fontVariables = fontList.map((font) => font.variable);
