9ab226df49b384213a05d31def0bed19d32ac822
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-10-04 16:17:33 +0600

    fix: nest backend formatting  issues

--- apps/backend/src/database/constants.ts
+++ apps/backend/src/database/constants.ts
@@ -1,3 +1,3 @@
 export const DBS = {
-	PRIMARY: 'primary',
+  PRIMARY: 'primary',
 } as const;
--- apps/backend/src/database/index.ts
+++ apps/backend/src/database/index.ts
@@ -1,11 +1,11 @@
-import { neon, neonConfig } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
-import ws from "ws";
-import * as schema from "./schema";
+import { neon, neonConfig } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import ws from 'ws';
+import * as schema from './schema';
 
 neonConfig.webSocketConstructor = ws;
 
-const sql = neon(process.env.DATABASE_URL || "");
+const sql = neon(process.env.DATABASE_URL || '');
 export const db = drizzle(sql, { schema });
 
 export { schema };
